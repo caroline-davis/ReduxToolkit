f23cf64f41571640ea73bf77bd8bc834d7c299de
Caroline Davis <>
2023-12-11 14:11:20 +1100

    feat: Add emoji reactions

---
+++ src/features/posts/ReactionButtons.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
+
+const reactionEmoji = {
+  thumbsUp: "👍",
+  wow: "😱",
+  heart: "💗",
+  rocket: "🚀",
+  coffee: "🍵",
+};
+
+const ReactionButtons = ({ post }) => {
+  const dispatch = useDispatch();
+
+  // the object look up to match the name with the emoji array above
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return (
+      <button
+        key={name}
+        type="button"
+        className="reactionButton"
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
+        {emoji} {post.reactions[name]}
+      </button>
+    );
+  });
+  return <div>{reactionButtons}</div>;
+};
+
+export default ReactionButtons;
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,35 +1,77 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
-    { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things."},
-    { id: '2', title: 'Slices....', content: "The more I say slice, the more I like it."}
-]
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+  {
+    id: "2",
+    title: "Slices....",
+    content: "The more I say slice, the more I like it.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+];
 
 // can mutate the state using immer (state.push) in reducers, otherwise u gota do the spread op ...props
 const postsSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers: {
-        postAdded: { 
-            reducer(state, action) {
-            state.push(action.payload)
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
             },
-            prepare(title, content, userId) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        title,
-                        content,
-                        userId
-                    }
-                }
-            }
-        }
-    }
-})
+          },
+        };
+      },
+    },
+    reactionAdded: (state, action) => {
+     console.log("Reaction Added:", action.payload);
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      console.log(existingPost, "exisiting post")
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    },
+  },
+});
 
 // the postsSlice.actions bit, the actions is auto made with same name instead of doing manually
-export const { postAdded } = postsSlice.actions
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export const selectAllPosts = (state) => state.posts;
 
