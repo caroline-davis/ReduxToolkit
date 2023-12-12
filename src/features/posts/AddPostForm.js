733c6aabcc2b4f9b1499b036e46243c86457ca3d
Caroline Davis <>
2023-12-12 15:24:05 +1100

    feat: Add api call

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -4,74 +4,65 @@ import { useDispatch, useSelector } from "react-redux";
 import { postAdded } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
-
 const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
 
-    const [title, setTitle] = useState("")
-    const [content, setContent] = useState("")
-    const [userId, setUserId] = useState("")
+  const users = useSelector(selectAllUsers);
 
-    const users = useSelector(selectAllUsers)
-    
-    const onTitleHasChanged = e => setTitle(e.target.value)
-    const onContentHasChanged = e => setContent(e.target.value)
-    const onAuthorHasChanged = e => setUserId(e.target.value)
-    const dispatch = useDispatch();
+  const onTitleHasChanged = (e) => setTitle(e.target.value);
+  const onContentHasChanged = (e) => setContent(e.target.value);
+  const onAuthorHasChanged = (e) => setUserId(e.target.value);
+  const dispatch = useDispatch();
 
-    const onSavePostClicked = () => {
-        if (title && content && userId) {
-            dispatch(
-                postAdded(title, content, userId)
-            )
-            setTitle("")
-            setContent("")
-            setUserId("")
-        }
+  const onSavePostClicked = () => {
+    if (title && content && userId) {
+      dispatch(postAdded(title, content, userId));
+      setTitle("");
+      setContent("");
+      setUserId("");
     }
+  };
 
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
-    const usersOptions = users.map(user => (
-        <option key={user.id} value={user.id}>
-            {user.name}
-        </option>
-    ))
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
-        <h2>Add a New Post</h2>
-        <form>
-            <label htmlFor="postTitle">Post Title:</label>
-            <input
-                type="text"
-                id="postTitle"
-                name="postTitle"
-                value={title}
-                onChange={onTitleHasChanged}
-            />
-            <label htmlFor="postAuthor">Author:</label>
-            <select
-                id="postAuthor" 
-                value={userId} 
-                onChange={onAuthorHasChanged}>
-                <option value=""></option>
-                {usersOptions}
-            </select>
-            <label htmlFor="postContent">Content:</label>
-            <textarea 
-                id="postContent"
-                name="postContent"
-                value={content}
-                onChange={onContentHasChanged}
-            />
-            <button 
-                type="button"
-                onClick={onSavePostClicked}
-                disabled={!canSave}
-            >Save Post</button>
-        </form>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleHasChanged}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorHasChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentHasChanged}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default AddPostForm
+export default AddPostForm;
--- src/features/posts/TimeAgo.js
+++ src/features/posts/TimeAgo.js
@@ -1,17 +1,17 @@
 import { parseISO, formatDistanceToNow } from "date-fns";
 
-import React from 'react'
+import React from "react";
 
 const TimeAgo = ({ timestamp }) => {
-    let timeAgo = ""
-    const date = parseISO(timestamp)
-    const timePeriod = formatDistanceToNow(date)
-    timeAgo = `${timePeriod} ago`
+  let timeAgo = "";
+  const date = parseISO(timestamp);
+  const timePeriod = formatDistanceToNow(date);
+  timeAgo = `${timePeriod} ago`;
   return (
     <span title={timestamp}>
-        &nbsp; <i>{timeAgo}</i>
+      &nbsp; <i>{timeAgo}</i>
     </span>
-  )
-}
+  );
+};
 
-export default TimeAgo
+export default TimeAgo;
